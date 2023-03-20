5986d5e873c4ba6e8e14101dea50663c17d77084
qtin0106 <qtin930106@gmail.com>
2023-03-20 17:08:06 +0800

    Create Boolean01.java
    Boolean

---
+++ Boolean01.java
@@ -0,0 +1,13 @@
+public class Boolean01{
+	public static void main(String[] args){
+		//判断成绩是否通过案例
+		// 定义一个布尔变量
+		boolean isPass = false;
+		if(isPass == true){
+			System.out.println("考试通过。恭喜");
+
+		} else{
+			System.out.println("考试没通过，下次努力");
+		}
+	}
+}
